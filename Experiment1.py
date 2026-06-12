6cea6eccc8deed640cfe2948cde485cf1adb2fa3
Coen <coen.buller@gmail.com>
2026-06-13 01:26:57 +0200

    Some comments and removed some papers

--- Experiment1.py
+++ Experiment1.py
@@ -7,7 +7,7 @@ from src.DART.DARTConfig import Config
 
 from Plotting.PlotExperiment1 import plot_all_experiments_combined
 
-"""Executes experiment 1 and plot the results. It stores the final figure under ./Results/Experiment_1"""
+"""Executes experiment 1 and plot the results. It stores the final figure under ./Results/Experiment_1. Parameters are varied are shown in individual experiment files."""
 
 EXPERIMENTS = {"Projection Ordering": RunAngleOrdering,
                "Smoothing": RunSmoothing,
